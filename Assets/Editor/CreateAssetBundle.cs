313ccc50c3ea464d93bb2e03a82e0898aa586f3c
MinamiChiwa <479325705@qq.com>
2022-04-16 21:56:12 +0800

    add

---
+++ Assets/Editor/CreateAssetBundle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class CreateAssetBundle
+{
+    [MenuItem("Assets/Build AssetBundle")]
+    static void BuildAssetBundle()
+    {
+        var assetBundleDirectory = "Assets/StreamingAssets";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
+    }
+}
